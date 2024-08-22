5f95acc28fd7626065ac51a7430e65ab42326198
Doctor <vonflow@gmail.com>
2024-08-22 02:11:22 -0600

    use mime module to check mime types

--- bot/src/util/findMediaAtPath.ts
+++ bot/src/util/findMediaAtPath.ts
@@ -1,22 +1,28 @@
 import type { PathLike } from 'node:fs'
 import type { MediaInfo } from '~/bot/types/types.js'
 import { parseFile } from 'music-metadata'
+import mime from 'mime/lite'
 
 export const findMediaAtPath = (
   path: PathLike,
 ): Promise<MediaInfo | undefined> => {
   return new Promise((resolve) => {
-    parseFile(path.toString())
-      // strip `native` and `quality` out of parsed result. note this throws @typescript-eslint/no-unused-vars
-      .then(({ native, quality, ...parsed }) =>
-        resolve({
-          path,
-          metadata: parsed,
-        }),
-      )
-      .catch(() => {
-        // console.error(err)
-        resolve(undefined)
-      })
+    const pathString = path.toString()
+    if (mime.getType(pathString).startsWith('audio/')) {
+      parseFile(pathString)
+        // strip `native` and `quality` out of parsed result. note this throws @typescript-eslint/no-unused-vars
+        .then(({ native, quality, ...parsed }) =>
+          resolve({
+            path,
+            metadata: parsed,
+          }),
+        )
+        .catch((err) => {
+          console.error(err)
+          resolve(undefined)
+        })
+    } else {
+      resolve(undefined)
+    }
   })
 }
