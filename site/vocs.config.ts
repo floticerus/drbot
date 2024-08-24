641bae37ba87716a82d06bbbcf7f3b6c10394419
Doctor <vonflow@gmail.com>
2024-08-24 16:17:02 -0600

    filling out docs

--- site/vocs.config.ts
+++ site/vocs.config.ts
@@ -17,8 +17,71 @@ export default defineConfig({
       link: '/running-without-docker',
     },
     {
+      text: 'Creating Discord Bot',
+      link: '/creating-discord-bot',
+    },
+    {
       text: 'Usage',
-      link: '/usage',
+      items: [
+        {
+          text: 'Slash Commands',
+          link: '/usage-slash-commands',
+          items: [
+            {
+              text: '/clear',
+              link: '/command-clear',
+            },
+            {
+              text: '/np',
+              link: '/command-np',
+            },
+            {
+              text: '/pause',
+              link: '/command-pause',
+            },
+            {
+              text: '/play',
+              link: '/command-play',
+            },
+            {
+              text: '/playall',
+              link: '/command-playall',
+            },
+            {
+              text: '/queue',
+              link: '/command-queue',
+            },
+            {
+              text: '/scan',
+              link: '/command-scan',
+            },
+            {
+              text: '/search',
+              link: '/command-search',
+            },
+            {
+              text: '/shuffle',
+              link: '/command-shuffle',
+            },
+            {
+              text: '/skip',
+              link: '/command-skip',
+            },
+            {
+              text: '/stats',
+              link: '/command-stats',
+            },
+            {
+              text: '/stop',
+              link: '/command-stop',
+            },
+          ].sort(({ text: textA }, { text: textB }) => {
+            const a = textA.toLowerCase()
+            const b = textB.toLowerCase()
+            return a > b ? 1 : a < b ? -1 : 0
+          }),
+        },
+      ],
     },
     {
       text: 'Troubleshooting',
