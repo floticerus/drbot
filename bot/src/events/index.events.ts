fa4de0586a1388071fa0a195bdca97bc5f718fa3
Kevin <vonflow@gmail.com>
2024-08-24 03:14:17 -0600

    add pluralize util, cleanup events (#9)

--- bot/src/events/index.events.ts
+++ bot/src/events/index.events.ts
@@ -1,18 +1,20 @@
 import events from './events.js'
+import { pluralize } from '~/bot/util/index.js'
 
 events.on('index:start', () => {
   console.log('Building index... this could take a little bit')
 })
 
 events.on('index:found', (/* _numResults: number */) => {
-  // console.log(`Indexing ${numResults} media file${numResults === 1 ? '' : 's'}`)
+  // console.log(`Indexing ${numResults} media ${pluralize(numResults, 'file', 'files')}}`)
 })
 
 events.on(
   'index:complete',
   ({ duration, numResults }: { duration: number; numResults: number }) => {
+    const seconds = duration / 1000
     console.log(
-      `Finished indexing ${numResults} file${numResults === 1 ? '' : 's'} in ${duration / 1000} seconds`,
+      `Finished indexing ${numResults} ${pluralize(numResults, 'file', 'files')}} in ${seconds} ${pluralize(seconds, 'second', 'seconds')}`,
     )
   },
 )
@@ -22,7 +24,7 @@ events.on(
   ({ duration, numDeleted }: { duration: number; numDeleted: number }) => {
     const seconds = duration / 1000
     console.log(
-      `Pruning removed ${numDeleted ? '~' : ''}${numDeleted} media entries in ${seconds} second${seconds === 1 ? '' : 's'}`,
+      `Pruning removed ${numDeleted ? '~' : ''}${numDeleted} media ${pluralize(numDeleted, 'entry', 'entries')} in ${seconds} ${pluralize(seconds, 'second', 'seconds')}}`,
     )
   },
 )
