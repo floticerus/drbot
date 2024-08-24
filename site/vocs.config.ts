47446d054bfc71cb13555a96531648fd0e980522
Kevin <vonflow@gmail.com>
2024-08-24 10:51:43 -0600

    begin adding vocs (#11)

---
+++ site/vocs.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vocs";
+
+export default defineConfig({
+  title: "drbot",
+  description: "A (local) music bot for discord",
+  sidebar: [
+    {
+      text: "Getting Started",
+      link: "/getting-started",
+    },
+    {
+      text: "Example",
+      link: "/example",
+    },
+  ],
+});
